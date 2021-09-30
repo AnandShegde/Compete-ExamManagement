105c1e2397d027599b2737f1e6603dd0f1c309a4
AnandShegde <anandishegde@gmail.com>
2021-09-30 18:55:20 +0530

    added required tag

--- signup.php
+++ signup.php
@@ -6,13 +6,13 @@
     <div class="SignUP">
         <h2>Sign UP</h2>
         <form action="#" method="post"> <!--Sign up-->
-            <span class="key">Username</span> <input type="text" name="Username"> <br>
-            <span class="key">Gmail ID</span> <input type="text" name="Email"> <br>
-            <span class="key">Age</span> <input type="number" name="Age"> <br>
-            <span class="key">Institute</span><input type="text" name="Institute"> <br>
-            <span class="key">Date of Birth</span><input type="date" name="DOB"> <br>
-            <span class="key">Create Password</span><input type="password" name="CrPassword"> <br>
-            <span class="key">Confirm Password</span><input type="text" name="CnPassword"> 
+            <span class="key">Username*</span> <input type="text" name="Username" required> <br>
+            <span class="key">Gmail ID*</span> <input type="text" name="Email" required> <br>
+            <span class="key">Age</span> <input type="number" name="Age" > <br>
+            <span class="key">Institute*</span><input type="text" name="Institute" required> <br>
+            <span class="key">Date of Birth</span><input type="date" name="DOB" required> <br>
+            <span class="key">Create Password*</span><input type="password" name="CrPassword" required> <br>
+            <span class="key">Confirm Password*</span><input type="text" name="CnPassword" required> 
             <button type="submit">Sign Up</button> 
         </form>
         <form action="index.php">
