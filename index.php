83752d64fca7564a1104fa85263fbc69e5fd5c3a
AnandShegde <anandishegde@gmail.com>
2021-10-01 10:01:11 +0530

    we can now login

--- index.php
+++ index.php
@@ -7,7 +7,31 @@
     <title>Document</title>
 </head>
 <body>
-<?php
+    <h1>DARE2COMPETE</h1>
+    <center><p id="errors"></p></center>
+    <div class="container">
+        <div  id='bgm'>
+        
+        </div>
+        <div class="Login">
+            <h2>Login</h2>
+            
+            <form action="" method="post"> <!--Login form-->
+                Email ID: <input type="text" name="Email" id="Gmail"> <br>
+                Password: <input type="password" name="Password"> 
+                <button type="submit" value="login" name='login'>Log in</button>
+                <button type="submit">Forgot Password</button> <br>
+            </form>
+            <form action="signup.php">
+                New User? <button type="submit">Sign Up</button>
+            </form>
+        </div>
+    </div>
+
+
+
+
+    <?php
     //pavan kumar
     $conn = mysqli_connect('localhost', 'root', '');
     $sqlQuery = 'CREATE DATABASE IF NOT EXISTS userInfo ;';
@@ -21,8 +45,8 @@
     if($login)
     {
         $Username= $_POST['Email'];
-        $password= $_POST['password'];
-        $query= "SELECT uName, uPasword FROM userTable WHERE uGmail='$Email'";
+        $password= $_POST['Password'];
+        $query= "SELECT * FROM userTable WHERE uGmail='$Username'";
         $result= $conn1->query($query);
         
         if($result->num_rows>0)
@@ -34,157 +58,24 @@
                 header("Location: homepage.html"); 
 
             }
-        }
-    }
-    
-    if($sign)
-    {
-        
-        $Username= $_POST['Username'];
-        $Email= $_POST['Email'];
-        $age= $_POST['Age'];
-        $Insti= $_POST['Institute'];
-        $password= $_POST['CrPassword'];
-
-
-
-        $sql= "SELECT uName FROM userTable WHERE uName='$Username'";
-        $sql1 ="SELECT uName FROM userTable WHERE uGmail='$Email'";
-        $result= $conn1->query($sql); // for checking whether given username already exists
-        $result1= $conn1->query($sql1);// for checking whether given email already exists
-        if($result->num_rows>0)
-        {
-            echo "Username Already exists";
-
-        }
-        elseif($result1->num_rows>0)
-        {
-            echo "Email already exists";
-            
-        }
-
-
-
-        else
-        {
-            $sql= "INSERT INTO userTable (uName,uGmail,uAge,uInstitute,uPassword) VALUES('$Username','$Email','$age','$Insti','$password');";
-            
-            if($conn1->query($sql))
+            else
             {
-                //echo 'Created the account successfully';
-                $msg = "Thank you for registering in our Exam management application\n\nFollowing are the information that u have entered\n username= $Username\n password= $password";
-                $subject= "Thanks for signing up";
-                // use wordwrap() if lines are longer than 70 characters
-                $msg = wordwrap($msg,70);
-                $txt = "Hello world!";
-                $headers = "From: webmaster@example.com" . "\r\n" .
-                "BCC: anandishegde@gmail.com";//200030041@iitdh.ac.in, 200010022@iitdh.ac.in
-                // send email
-                
-                if(mail("$Email",$subject,$msg,$headers))
-                {
-                    //echo 'mailed successfully';
-                    
-                }
-                else
-                {
-                    echo "sesesfes";
-                }
+                ?>
+            <script>document.getElementById('errors').innerHTML="The emailid and password didn't match"</script>
+
+            <?php
             }
         }
+        else
+        {
+            ?>
+            <script>document.getElementById('errors').innerHTML="There is no account with given email id"</script>
 
+            <?php
+        }
     }
-    
-//    $conn = mysqli_connect('localhost', 'root', '');
-//    $sql = 'CREATE DATABASE IF NOT EXISTS publications;';
-//    mysqli_query($conn, $sql);
-//    $conn1 = mysqli_connect('localhost', 'root', '', 'publications');
-//    $query1 = 'CREATE TABLE IF NOT EXISTS authors(
-//        author VARCHAR(120) NOT NULL, publisher VARCHAR(30)
-//        ); ';
-//    $query2 = 'CREATE TABLE IF NOT EXISTS titles( 
-//        title VARCHAR(120) NOT NULL, author VARCHAR(120), year SMALLINT(6));';
-//    mysqli_query($conn1, $query1);
-//    mysqli_query($conn1, $query2);
+
 ?> 
 
-    <h1>DARE2COMPETE</h1>
-    <div class="container">
-        <div class="child" id='bgm'>
-        
-        </div>
-        <div class="child">
-            <h2>Login</h2>
-            <form action="" method="post"> <!--Login form-->
-                Email ID: <input type="text" name="Email" id="Gmail"> <br>
-                Password: <input type="password" name="Password"> 
-                <button type="submit" value="login">Log in</button>
-                <button type="submit">Forgot Password</button> <br>
-            </form>
-            <form action="signup.php">
-                New User? <button type="submit">Sign Up</button>
-            </form>
-        </div>
-    </div>
-<!-- <?php
-    if($sign)
-    {
-        
-        $Username= $_POST['Username'];
-        $Email= $_POST['Email'];
-        $age= $_POST['Age'];
-        $Insti= $_POST['Institute'];
-        $password= $_POST['CrPassword'];
-        $sql= "SELECT uName FROM userTable WHERE uName='$Username'";
-        $result= $conn1->query($sql);
-        
-
-        if($result->num_rows>0)
-        {
-            echo "Username Already exists";
-
-        }
-        else
-        {
-            $sql= "INSERT INTO userTable (uName,uGmail,uAge,uInstitute,uPassword) VALUES('$Username','$Email','$age','$Insti','$password');";
-            
-            if($conn1->query($sql))
-            {
-                //echo 'Created the account successfully';
-                $msg = "Thank you for registering in our Exam management application\n\nFollowing are the information that u have entered\n username= $Username\n password= $password";
-                $subject= "Thanks for signing up";
-                // use wordwrap() if lines are longer than 70 characters
-                $msg = wordwrap($msg,70);
-                $txt = "Hello world!";
-                $headers = "From: webmaster@example.com" . "\r\n" .
-                "BCC: anandishegde@gmail.com";//200030041@iitdh.ac.in, 200010022@iitdh.ac.in
-                // send email
-                
-                if(mail("$Email",$subject,$msg,$headers))
-                {
-                    //echo 'mailed successfully';
-                    
-                }
-                else
-                {
-                    echo "sesesfes";
-                }
-            }
-        }
-
-    }
-    
-//    $conn = mysqli_connect('localhost', 'root', '');
-//    $sql = 'CREATE DATABASE IF NOT EXISTS publications;';
-//    mysqli_query($conn, $sql);
-//    $conn1 = mysqli_connect('localhost', 'root', '', 'publications');
-//    $query1 = 'CREATE TABLE IF NOT EXISTS authors(
-//        author VARCHAR(120) NOT NULL, publisher VARCHAR(30)
-//        ); ';
-//    $query2 = 'CREATE TABLE IF NOT EXISTS titles( 
-//        title VARCHAR(120) NOT NULL, author VARCHAR(120), year SMALLINT(6));';
-//    mysqli_query($conn1, $query1);
-//    mysqli_query($conn1, $query2);
-?> -->
 </body>
 </html>
--- signup.php
+++ signup.php
@@ -5,18 +5,96 @@
 <body>
     <div class="SignUP">
         <h2>Sign UP</h2>
-        <form action="#" method="post"> <!--Sign up-->
+        <form action="" method="post"> <!--Sign up-->
             <span class="key">Username*</span> <input type="text" name="Username" required> <br>
             <span class="key">Gmail ID*</span> <input type="text" name="Email" required> <br>
-            <span class="key">Age</span> <input type="number" name="Age" > <br>
-            <span class="key">Institute*</span><input type="text" name="Institute" required> <br>
-            <span class="key">Date of Birth</span><input type="date" name="DOB" required> <br>
-            <span class="key">Create Password*</span><input type="password" name="CrPassword" required> <br>
-            <span class="key">Confirm Password*</span><input type="text" name="CnPassword" required> 
-            <button type="submit">Sign Up</button> 
+            <span class="key">Age</span> <input type="number" name="Age"  required> <br>
+            <span class="key">Institute*</span><input type="text" name="Institute"  required> <br>
+            <span class="key">Date of Birth</span><input type="date" name="DOB" > <br>
+            <span class="key">Create Password*</span><input type="password" name="CrPassword"  required> <br>
+            <span class="key">Confirm Password*</span><input type="text" name="CnPassword"  required> 
+            <input value="Sign Up" name="sign"type="submit"></input> 
         </form>
         <form action="index.php">
             <button type="submit">Back</button>
         </form>
     </div>
+
+
+
+
+
+<?php
+$conn = mysqli_connect('localhost', 'root', '');
+$sqlQuery = 'CREATE DATABASE IF NOT EXISTS userInfo ;';
+mysqli_query($conn, $sqlQuery);
+$conn1 = mysqli_connect('localhost', 'root', '', 'userInfo');
+$query = 'CREATE TABLE IF NOT EXISTS userTable(
+    uName VARCHAR(120) NOT NULL, uGmail VARCHAR(200) NOT NULL, uAge INT NOT NULL, uInstitute VARCHAR(120) NOT NULL, uPassword VARCHAR(50) ); ';
+mysqli_query($conn1, $query);
+@$sign= $_POST['sign'];
+
+if($sign)
+{
+    
+    $Username= $_POST['Username'];
+    $Email= $_POST['Email'];
+    $age= $_POST['Age'];
+    $Insti= $_POST['Institute'];
+    $password= $_POST['CrPassword'];
+
+
+
+    $sql= "SELECT uName FROM userTable WHERE uName='$Username'";
+    $sql1 ="SELECT uName FROM userTable WHERE uGmail='$Email'";
+    $result= $conn1->query($sql); // for checking whether given username already exists
+    $result1= $conn1->query($sql1);// for checking whether given email already exists
+    if($result->num_rows>0)
+    {
+        echo "Username Already exists";
+
+    }
+    elseif($result1->num_rows>0)
+    {
+        echo "Email already exists";
+        
+    }
+
+
+
+    else
+    {
+        $sql= "INSERT INTO userTable (uName,uGmail,uAge,uInstitute,uPassword) VALUES('$Username','$Email','$age','$Insti','$password');";
+        
+        if($conn1->query($sql))
+        {
+            //echo 'Created the account successfully';
+            $msg = "Thank you for registering in our Exam management application\n\nFollowing are the information that u have entered\n username= $Username\n password= $password";
+            $subject= "Thanks for signing up";
+            // use wordwrap() if lines are longer than 70 characters
+            $msg = wordwrap($msg,70);
+            $txt = "Hello world!";
+            $headers = "From: webmaster@example.com" . "\r\n" .
+            "BCC: anandishegde@gmail.com";//200030041@iitdh.ac.in, 200010022@iitdh.ac.in
+            // send email
+            
+            if(mail("$Email",$subject,$msg,$headers))
+            {
+                echo 'mailed successfully';
+                header('Location:index.php');
+                
+                
+            }
+            else
+            {
+                echo "sesesfes";
+            }
+            header('Location:index.php');
+            
+        }
+    }
+
+}
+
+?>
 </body>
