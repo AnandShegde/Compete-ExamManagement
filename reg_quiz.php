a5718249320c8c21802d3d867ed09d2a9f320ee0
pavanvpatil <pavanvpatil01@gmail.com>
2021-11-01 10:50:22 +0530

    ERROR REMOVED

--- reg_quiz.php
+++ reg_quiz.php
@@ -11,23 +11,34 @@
     $row = $result->fetch_assoc();
     $username=$row['uName'];
  
-    $conn = mysqli_connect("localhost", "root", "", $username);
-    $sql= "CREATE TABLE IF NOT EXISTS 'reg_quizes' ( 
+    $conn = mysqli_connect("localhost", "root", "",$username);
+    if(!$conn)
+    {
+        echo "cool";
+    }
+    $sql= "CREATE TABLE IF NOT EXISTS reg_quizes( 
         id INT NOT NULL ,
         host TEXT NOT NULL ,
-        'date' DATE NOT NULL , 
-        'start time' TIME NOT NULL , 
-        'end time' TIME NOT NULL , 
+        q_date DATE NOT NULL , 
+        starttime TIME NOT NULL , 
+        endtime TIME NOT NULL , 
         duration TIME NOT NULL ,
-        'name' TEXT NOT NULL );";
+        q_name TEXT NOT NULL )";
     mysqli_query($conn, $sql);
-    $id = $_POST['submitted'];
-    
-    
-    $sql = "SELECT * FROM quizes WHERE id = '$id'";
-    $result = mysqli_query($connect, $sql);
-    $data = mysqli_fetch_all($result, MYSQLI_ASSOC);
-    echo $data[0]['name'];
-    $sql = "INSERT INTO reg_quizes VALUES(`$data[0]['id']`, `$data[0]['host-Email']`, `$data[0]['date']`, `$data[0]['start time']`, `$data[0]['end time']`, `$data['duration'][0]`, `$data[0]['name']`)";
-    mysqli_query($conn, $sql);  
+
+    $id=$_POST['submitted'];
+    $sql="SELECT * FROM quizes WHERE id = '$id'";
+    $result=mysqli_query($connect, $sql);
+    $data=$result->fetch_assoc();
+
+    $q_name=$data['name'];
+    $host=$data['host-Email'];
+    $q_date=$data['date'];
+    $starttime=$data['start time'];
+    $endtime=$data['end time'];
+    $duration=$data['duration'];
+
+    $sql = "INSERT INTO reg_quizes (id,host,q_date,starttime,endtime,duration,q_name)  VALUES('$id','$host','$q_date','$starttime','$endtime','$duration','$q_name')";
+    mysqli_query($conn, $sql);
+    echo "registered successfully";
 ?>
